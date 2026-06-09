755eff07386f77f6b83ca3a9b09313180b4fbc93
Batdrew1233 <drewstevenson1233@gmail.com>
2026-06-09 14:09:38 -0400

    Seed and read in the runner

--- src/main/java/com/plurasight/sneakerdrops/StartupRunner.java
+++ src/main/java/com/plurasight/sneakerdrops/StartupRunner.java
@@ -1,24 +1,41 @@
 package com.plurasight.sneakerdrops;
 
+import com.plurasight.sneakerdrops.data.BrandRepository;
+import com.plurasight.sneakerdrops.models.Brand;
 import com.plurasight.sneakerdrops.service.DropService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class StartupRunner implements CommandLineRunner {
 
-    private final DropService dropService;
+    private final BrandRepository brandRepository;
 
     @Autowired
-    public StartupRunner(DropService dropService){
-        this.dropService = dropService;
+    public StartupRunner(BrandRepository brandRepository){
+        this.brandRepository = brandRepository;
     }
 
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println(dropService.getStatus());
+        seedData();
+        for(Brand b : brandRepository.findAll()){
+            System.out.println(b.getId() + " - " + b.getName());
+        }
 
+
+    }
+
+    private void seedData(){
+        if(brandRepository.count() == 0){
+            brandRepository.save(new Brand("Nike"));
+            brandRepository.save(new Brand("Adidas"));
+            brandRepository.save(new Brand("New Balance"));
+
+        }
     }
 }
